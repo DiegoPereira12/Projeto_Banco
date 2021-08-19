449e34e22af884482921f7e446048de30213cfb0
DiegoPereira12 <diegoo.junior55@gmail.com>
2021-08-19 15:13:35 -0300

    ajustes realizados no modulo cliente

--- modulo/cliente.py
+++ modulo/cliente.py
@@ -1,13 +1,22 @@
 class Cliente(object):
     
+    contador = 0
+
     def __init__(self, nome, cpf, data_nascimento):
+        self.codigo = Cliente.contador
         self.nome = nome
         self.__cpf = cpf
         self.data_nascimento = data_nascimento
-      
+        Cliente.contador +=1
+
+
+    def retorna_codigo(self):
+        return self.codigo
+          
     def retorna_nome(self):
         return self. nome
     
+    @property
     def retorna_cpf(self):
         return self.__cpf
     
