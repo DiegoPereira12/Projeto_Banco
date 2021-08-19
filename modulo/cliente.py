793ec0736cefed146c24762b402ccac4e2686800
DiegoPereira12 <diegoo.junior55@gmail.com>
2021-08-19 00:49:11 -0300

    Nova atualização realizada cliente / conta

--- modulo/cliente.py
+++ modulo/cliente.py
@@ -1,23 +1,16 @@
 class Cliente(object):
     
-    contador: int = 0
-
     def __init__(self, nome, cpf, data_nascimento):
-        self.codigo = Cliente.contador
-        self.nome  = nome
+        self.nome = nome
         self.__cpf = cpf
         self.data_nascimento = data_nascimento
-        Cliente.contador += 1
-
-    def codigo(self):
-        return self.codigo
-
-    def nome(self):
-        return self.nome
-
-    @property
-    def cpf(self):
+      
+    def retorna_nome(self):
+        return self. nome
+    
+    def retorna_cpf(self):
         return self.__cpf
-
-    def dat_nascimento(self):
+    
+    def retorna_datanascimento(self):
         return self.data_nascimento
+    
--- modulo/conta.py
+++ modulo/conta.py
@@ -1,29 +1,24 @@
-from typing import NoReturn
 from cliente import Cliente
 
 class Conta(object):
 
-    codigo: 1001
-
-    def __init__(self, numero, cliente: Cliente, saldo, limite): 
+   def __init__(self, numero, saldo, limite):
         self.numero = numero
-        self.cliente: Cliente = cliente
         self.__saldo = 0.0
         self.__limite = 100.0
-        Conta.codigo +=1
-
-    def numero(self):
+        
+    def retorna_numero(self):
         return self.numero
 
-    def cliente(self):
+    def retorna_cliente(self):
         return self.cliente
     
     @property
-    def saldo(self):
+    def retorna_saldo(self):
         return self.__saldo
     
     @property
-    def limite(self):
+    def retorna_limite(self):
         return self.__limite
 
 
