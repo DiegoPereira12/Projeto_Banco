dabf7c9a689079d1e12cd34067555e0227291e1c
DiegoPereira12 <diegoo.junior55@gmail.com>
2021-08-25 11:48:35 -0300

    novas funções adcionadas

--- modulo/conta.py
+++ modulo/conta.py
@@ -1,31 +1,49 @@
 from modulo.cliente import Cliente
 
-class Conta():
+class Conta(Cliente):
 
     codigo = 1001
 
-    def __init__(self, nome, cpf, data_nascimento):
+    def __init__(self, nome, cpf, data_nascimento, saldo_total):
         self.numero = Conta.codigo
         super().__init__(nome, cpf, data_nascimento)
         self.__saldo = 0.0
         self.__limite = 100.0
+        self.__saldo_total = saldo_total
         Conta.codigo += 1
         
     def retorna_numero(self):
         return self.numero
     
+    def retorn_cliente(self):
+        return super(Cliente)
+    
     @property
     def retorna_saldo(self):
         return self.__saldo
+    
+    @retorna_saldo.setter
+    def retorna_saldo(self, valor):
+        self.__saldo = valor
     
     @property
     def retorna_limite(self):
         return self.__limite
     
-    def dados(self):
-        print(f' O %s  tem o cpf %s é nasceu no dia %s é sua conta bancária %s com o saldo de %s' %(self.nome, self.__cpf, self.numero, self.__saldo))
+    @retorna_limite.setter
+    def retorna_limite(self, valor):
+        self.__limite = valor
     
-    nome = Cliente('Diego')
-    cpf = Cliente('0722451725')
-    data_nascimento = Cliente('12071989')
+    @property
+    def retorna_saldo_total(self):
+        return self.__saldo_total
     
+    @retorna_saldo_total.setter
+    def retorna_saldo_total (self, valor):
+        self.__saldo_total= valor
+    
+    
+
+
+    
+
