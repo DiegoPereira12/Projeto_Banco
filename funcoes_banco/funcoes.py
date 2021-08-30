ee91a092415436e6657692847cc951d311fcc9b1
DiegoPereira12 <diegoo.junior55@gmail.com>
2021-08-30 11:31:46 -0300

    incluido funções do banco

--- funcoes_banco/funcoes.py
+++ funcoes_banco/funcoes.py
@@ -1,9 +1,30 @@
-def criar_conta():
-    
-nome = int(input('Nome do cliente: '))
-email = int(input('E-mail do cliente: '))
-cpf = int(input('CPF do cliente: '))
-data_nascimento = int(input('Data nascimento do cliente: '))
+from time import sleep
+from modulo.cliente import Cliente
+from modulo.conta import Conta
+import principal
 
-    
+
+contas = []
+
+def criar_conta():
+    print('Informe os dados do cliente')
+
+    nome = input('Digite o Nome do cliente => ')
+    cpf = input('Digite o CPF do cliente => ')
+    data_nascimento = input('Digite  a Data de Nascimento do cliente => ')
+
+    cliente: Cliente = Cliente(nome, cpf, data_nascimento)
+
+    conta: Conta = Conta(cliente)
+
+    contas.append(conta)
+
+    print('Sua conta foi criada com sucesso')
+    print('------------------')
+    print(conta)
+    print('------------------')
+    sleep(2)
+    principal.menu()
+
+
     
